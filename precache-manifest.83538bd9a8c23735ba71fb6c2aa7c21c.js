cdd82402a709adae4f5245d3d1b2060639f260cc
Johnny Howe <jonathon.o.howe@gmail.com>
2023-05-15 12:44:50 +1200

    Updates

---
+++ precache-manifest.83538bd9a8c23735ba71fb6c2aa7c21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d78661c9494f65abac02",
+    "url": "/drifto-pages/static/css/main.b18e60c4.chunk.css"
+  },
+  {
+    "revision": "d78661c9494f65abac02",
+    "url": "/drifto-pages/static/js/main.d78661c9.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/drifto-pages/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "152d043615f37f80bc43",
+    "url": "/drifto-pages/static/js/2.152d0436.chunk.js"
+  },
+  {
+    "revision": "42f8dfb5665e2fde717c",
+    "url": "/drifto-pages/static/js/runtime~main.42f8dfb5.js"
+  },
+  {
+    "revision": "ade076cfa43d736b6ecd2162b45c0cfb",
+    "url": "/drifto-pages/index.html"
+  }
+];
